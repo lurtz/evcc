0ea8f4b7ec777346e62fd98429a9898d4553f96e
lurtz <727209+lurtz@users.noreply.github.com>
2025-10-19 13:19:36 +0200

    Podman compatible container build
    - Dummy hidden *.ts file
    
    The glob pattern finds in a clean checkout only files in .storybook,
    which should be ignored per .dockerignore.
    
    Is Docker silent here or are files missing?
    
    - Add Podman target to Makefile
    
    Podman tries to be a drop in replacement for Docker, but not 100%. Thus
    the command is a little bit different.

---
+++ .podman-workaround.ts
@@ -0,0 +1,9 @@
+// Dockerfile specifies to copy hidden *.ts and *.mts files.
+// The only currently present files matching this pattern are in the
+// .storybook directory, which is excluded via .dockerignore.
+// I decided against modifying the Dockerfile,
+// because evcc might add files like that again and then I might have a harder to debug issue.
+
+export default {
+  no_use: 100,
+};

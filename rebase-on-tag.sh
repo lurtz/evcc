a017235a341c54529c33bc9c9b87feaeb0503f0a
Lutz <727209+lurtz@users.noreply.github.com>
2026-08-16 00:36:21 +0200

    restore branch when done

--- rebase-on-tag.sh
+++ rebase-on-tag.sh
@@ -14,6 +14,9 @@ if git ls-remote --exit-code --tags origin "refs/tags/${latest_tag}-lurtz" > /de
 	exit 0
 fi
 
+# save current branch to return to it later
+current_branch=$(git branch --show-current)
+
 # Add evcc remote if it doesn't exist
 git remote get-url evcc > /dev/null 2>&1 || git remote add evcc https://github.com/evcc-io/evcc.git
 
@@ -27,7 +30,10 @@ trap 'rm -rf "$tmp_dir"' EXIT
 
 git format-patch --output-directory "$tmp_dir" evcc/master..origin/master-lurtz
 git checkout "$latest_tag"
-git am --3way "$tmp_dir"/*.patch || { echo "Patch application failed. Aborting."; exit 1; }
+git am --3way "$tmp_dir"/*.patch || {
+	echo "Patch application failed. Aborting."
+	exit 1
+}
 git tag $latest_tag-lurtz
 
 if [ "$push_enabled" = "--push" ]; then
@@ -35,6 +41,11 @@ if [ "$push_enabled" = "--push" ]; then
 	git push origin $latest_tag-lurtz
 else
 	echo "Push is disabled. Skipping push of tag ${latest_tag}-lurtz to origin."
+fi
+
+# return to the original branch
+if [ -n "$current_branch" ]; then
+	git switch "$current_branch"
 fi
 
 exit 0

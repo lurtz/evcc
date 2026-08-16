f57ec32b5f611a72097060d3be9dbb0c2cf42bfc
Lutz <727209+lurtz@users.noreply.github.com>
2026-08-16 10:06:44 +0000

    make linter happy with podman workaround

--- .podman-workaround.ts
+++ .podman-workaround.ts
@@ -3,3 +3,7 @@
 // .storybook directory, which is excluded via .dockerignore.
 // I decided against modifying the Dockerfile,
 // because evcc might add files like that again and then I might have a harder to debug issue.
+
+export default {
+  no_use: 100,
+};
